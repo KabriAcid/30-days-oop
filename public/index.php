03b363ac3b489515138ab752d2719ddf5b14f0a8
Abdullahi Kabri <kabriacid01@gmail.com>
2025-01-11 03:34:24 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -1,17 +1,39 @@
 <?php
 
-interface Logger {
-    public function log($message);
+interface vehicleTraits
+{
+    public function Honk();
+    public function Brake();
+    public function Steer();
 }
 
-class MessageApp {
-    private $message;
-    public function __construct($message) {
-        $this->message = $message;
+class Car implements vehicleTraits
+{
+    public $model;
+    public $color;
+    public $speed;
+
+    public function __construct($model, $color = "", $speed)
+    {
+        $this->model = $model;
+        $this->color = $color;
+        $this->speed = $speed;
     }
-    public function Logger() {
-        return $this->message;
+
+    public function Honk()
+    {
+        echo "Beep Beep!\n";
+    }
+    public function Brake()
+    {
+        echo "Screech!\n";
+    }
+    public function Steer()
+    {
+        echo "Turning...\n";
     }
 }
-
-$message = new MessageApp("Hello World");
+$car = new Car('E350', "Green", 230);
+echo $car->Honk();
+echo $car->Brake();
+echo $car->Steer();
