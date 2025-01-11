64595a253951aa281a3d4f73d32752366a9a9564
Abdullahi Kabri <kabriacid01@gmail.com>
2025-01-11 01:21:44 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -1,13 +1,90 @@
-<title>OOP in PHP</title>
 <?php
+    // SIMPLE CLASS
+class Person {
+    public $name;
+    public $age;
 
-require '../autoload.php';
+    public function introduce(){
+        return "Hello my name is $this->name and I am $this->age years old";
+    }
+}
+$person = new Person('John Doe', 25);
+echo $person->introduce();
 
-use App\Controllers\UserController;
+// ENCAPSULATION
+class BankAccount {
+    private $balance = 0;
 
-$controller = new UserController();
-$user = $controller->index();
+    public function deposit($amount){
+        $this->balance+= $amount;
+    }
+    public function withdraw($amount){
+        if($this->balance < $amount){
+            return "Insufficient balance";
+        }
+        $this->balance -= $amount;
+    }
+    public function getBalance() {
+        return $this->balance;
+    }
+}
 
-$data = $user;
+$account = new BankAccount();
+$account->deposit(100);
+$account->withdraw(50);
+echo $account->getBalance();
 
-include '../app/Views/user.php';
+class Animal {
+    public $name;
+
+    public function makeSound(){
+        return 'Some sound';
+    }
+}
+
+class Dog extends Animal {
+    public function makeSound(){
+        return 'Bark';
+    }
+}
+class Cat extends Animal {
+    public function makeSound(){
+        return 'Meow';
+    }
+}
+
+$dog = new Dog();
+$cat = new Cat();
+
+echo $dog->makeSound();
+echo $cat->makeSound();
+
+// INTERFACE
+
+interface Shape {
+    public function area();
+}
+
+class Circle implements Shape {
+    private $radius;
+
+    public function __construct($radius){
+        $this->radius = $radius;
+    }
+    public function area(){
+        return 3.14 * $this->radius * $this->radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height){
+        $this->width = $width;
+        $this->height = $height;
+    }
+    public function area(){
+        return $this->width * $this->height;
+    }
+}
