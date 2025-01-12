3812681d4dc2cf2b393c774809e1477239732570
Abdullahi Kabri <kabriacid01@gmail.com>
2025-01-12 06:44:44 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>OOP</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
     <style>
         button {
